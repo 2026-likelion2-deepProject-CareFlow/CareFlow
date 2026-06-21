26b3b2694917348c7763b580f4e148f93d9317e4
HoJun-Seo <ghwns6659@gmail.com>
2026-06-22 00:31:48 +0900

    Refactor : getAgency API 에서 필요없는 매개변수 삭제

--- src/main/java/com/careflow/agency/controller/AgenciesController.java
+++ src/main/java/com/careflow/agency/controller/AgenciesController.java
@@ -34,8 +34,7 @@ public class AgenciesController {
     기존에 데이터가 있는 경우 데이터 반환 및 대행사 회원가입 -> 관리자 계정요청으로 서비스 넘어감
      */
     @GetMapping("/agencies")
-    public ResponseEntity<Agencies> getAgency(@Valid @RequestParam(name = "name") String agencyName,
-                                              @Valid @RequestParam(name = "businessNumber") String businessNumber) {
+    public ResponseEntity<Agencies> getAgency(@Valid @RequestParam(name = "businessNumber") String businessNumber) {
 
         // 대행사 계정 회원가입 시도 중 대행사 조회 결과 200 ok 반환될 경우 프론트에서 flag = 1 설정
         // 대행사 조회 결과가 존재하지 않아 NoSuchElementException 발생 및 NotFound 반환 시 flag = 0 설정
