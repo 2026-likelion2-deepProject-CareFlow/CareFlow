8bd77a327056fa35ed33bfdfaa18e58872e33996
HoJun-Seo <ghwns6659@gmail.com>
2026-06-22 17:26:44 +0900

    Refactor : 수리기사 계정 생성 요청 예외 메ì시지 수정

--- src/main/java/com/careflow/engineer/service/EngineerProfileService.java
+++ src/main/java/com/careflow/engineer/service/EngineerProfileService.java
@@ -103,11 +103,11 @@ public class EngineerProfileService {
             accountRequestId = accountRequestsRepository.save(accountRequests).getId();
 
         } else if (agencies.getApprovalStatus() == AgencyStatus.PENDING){
-            throw new IllegalStateException("아직 등록 대기중인 대행사 입니다. 등록이 된 이후 다시 요청해주세요.");
+            throw new IllegalStateException("아직 등록 대기중인 수리 기사입니다. 등록이 된 이후 다시 요청해주세요.");
         } else if (agencies.getApprovalStatus() == AgencyStatus.REJECTED){
             // AgencyStatus.REJECTED : 대행사에 대해 슈퍼 계정을 요청했으나 거부당한 경우
             // businessNumber 가 unique 제약조건을 가지고 있기 때문에 같은 대행사에 대해 중복으로 슈퍼 계정 생성 요청은 불가능
-            throw new IllegalStateException("등록이 거부된 대행사 입니다. 관리자에게 문의해주세요.");
+            throw new IllegalStateException("등록이 거부된 수리 기사입니다. 관리자에게 문의해주세요.");
         }
         return accountRequestId;
 
