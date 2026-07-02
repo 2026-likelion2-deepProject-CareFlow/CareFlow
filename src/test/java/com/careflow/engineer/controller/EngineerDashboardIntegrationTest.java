3e53df6e51665283514bc60bea4c3d91de5b4b23
hmhmhm <hyemin9973@gmail.com>
2026-07-02 18:45:01 +0900

    Fix : BankAccount 엔티티 팩토리 메서드 사용

--- src/test/java/com/careflow/engineer/controller/EngineerDashboardIntegrationTest.java
+++ src/test/java/com/careflow/engineer/controller/EngineerDashboardIntegrationTest.java
@@ -11,6 +11,7 @@ import com.careflow.as_request.repository.AsRequestRepository;
 import com.careflow.assignment.entity.AsAssignment;
 import com.careflow.assignment.repository.AsAssignmentRepository;
 import com.careflow.auth.security.JwtProvider;
+import com.careflow.bank_account.entity.BankAccount;
 import com.careflow.bank_account.repository.BankAccountRepository;
 import com.careflow.common.enums.AgencyStatus;
 import com.careflow.common.enums.AssignType;
@@ -90,7 +91,7 @@ class EngineerDashboardIntegrationTest {
         EngineerProfile profile = EngineerProfile.createInitial(engineer);
         profile.completeProfile(categoryRepository.save(ApplianceCategory.createRoot("세탁기", 1)), 2015, SkillLevel.ADVANCED, "반갑습니다");
         engineerProfileRepository.save(profile);
-        bankAccountRepository.save(BankAccount.builder().engineer(engineer).bankName("국민은행").accountNumber("123-456-789").build());
+        bankAccountRepository.save(BankAccount.create(engineer.getId(), "국민은행", "123-456-789", engineer.getName()));
 
         // 3. A/S 요청 -> 배정 -> 완료 -> 보고서 작성 흐름 세팅
         ApplianceCategory cat = categoryRepository.save(ApplianceCategory.createRoot("냉장고", 1));
